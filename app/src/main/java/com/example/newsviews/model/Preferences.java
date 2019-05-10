aa897b4032d49b8853ee1b7d5ed5e6a73b3c9875
Tusar0003 <faisalahmedtusar@gmail.com>
2019-05-11 01:25:31 +0600

    Add: Added introduction splash screen and regular splash screen.

---
+++ app/src/main/java/com/example/newsviews/model/Preferences.java
@@ -0,0 +1,31 @@
+package com.example.newsviews.model;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Preferences {
+
+    private static final String PREFERENCE_NAME = "Preferences";
+    private static final String PREFERENCE_KEY_STATUS = "KeyHost";
+
+    private Context mContext;
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor editor;
+
+    public Preferences(Context context) {
+        this.mContext = context;
+        mPreferences = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE);
+        editor = mPreferences.edit();
+    }
+
+    // Setting the status whether the user is launching app for the first time or not
+    public void setStatus() {
+        editor.putBoolean(PREFERENCE_KEY_STATUS, false);
+        editor.apply();
+    }
+
+    // Getting the status
+    public boolean isFirstTime() {
+        return mPreferences.getBoolean(PREFERENCE_KEY_STATUS, true);
+    }
+}
--- app/src/main/java/com/example/newsviews/presenter/activityPresenter/IntroductionPresenter.java
+++ app/src/main/java/com/example/newsviews/presenter/activityPresenter/IntroductionPresenter.java
@@ -1,4 +1,85 @@
 package com.example.newsviews.presenter.activityPresenter;
 
+import android.content.Context;
+import android.text.Html;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.newsviews.R;
+import com.example.newsviews.model.Preferences;
+
 public class IntroductionPresenter {
+
+    private static final String TAG = "IntroductionPresenter";
+
+    private Context mContext;
+    private View mView;
+
+    public IntroductionPresenter(View view) {
+        this.mView = view;
+        mContext = (Context) view;
+    }
+
+    public void initializeDotsView() {
+        mView.initializeDotsView();
+    }
+
+    public void addDotsIndicator(TextView[] mDots, int position) {
+        mView.removeAllViews();
+
+        for (int i=0;i<mDots.length;i++) {
+            mDots[i] = new TextView(mContext);
+            mDots[i].setText(Html.fromHtml("&#8226"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(mContext.getResources().getColor(R.color.colorTransparentWhite));
+
+            mView.addDotsViews(mDots[i]);
+        }
+
+        if (mDots.length > 0) {
+            mView.setDotColorToWhite(mDots[position]);
+        }
+    }
+
+    public void changeButtonStatus(int position, TextView[] mDots) {
+        if (position == 0) {
+            mView.disablePreviousButton();
+        } else if (position == mDots.length - 1) {
+            mView.changeNextButtonStatus();
+        } else {
+            mView.enableBothButton();
+        }
+    }
+
+    public void checkButtonStatus(String buttonStatus) {
+        if (buttonStatus.equalsIgnoreCase("Finish")) {
+            Log.e(TAG, "checkButtonStatus: ");
+
+            Preferences preferences = new Preferences(mContext);
+            preferences.setStatus();
+
+            mView.goToHomeActivity();
+        }
+    }
+
+    public void checkLaunchStatus() {
+        Preferences preferences = new Preferences(mContext);
+        Log.e(TAG, "checkLaunchStatus: " + preferences.isFirstTime());
+
+        if (!preferences.isFirstTime()) {
+            mView.goToHomeActivity();
+        }
+    }
+
+    public interface View {
+        void removeAllViews();
+        void initializeDotsView();
+        void addDotsViews(TextView dotView);
+        void setDotColorToWhite(TextView dotView);
+        void disablePreviousButton();
+        void changeNextButtonStatus();
+        void enableBothButton();
+        void goToHomeActivity();
+    }
 }
